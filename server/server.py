84f8d5fd116b39b883618f9bb60080a045912b58
Renan Santana <renan_ota@hotmail.com>
2020-05-22 21:30:25 -0300

    Refatora processo de check

--- server/server.py
+++ server/server.py
@@ -1,4 +1,6 @@
 import os
+import requests
+from server.bot import Bot
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 
@@ -8,6 +10,20 @@ mongo_uri = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/teste')
 mongo_uri += '?retryWrites=false'
 app.config['MONGO_URI'] = mongo_uri
 mongo = PyMongo(app)
+telegram_bot = Bot()
+
+def is_online(url):
+    """
+    Faz um GET no client para verificar se ele está online
+    """
+    complete_url = url + '/check'
+    try:
+        response = requests.get(complete_url)
+        if response.status_code == 200:
+            return True
+        return False
+    except:
+        return False
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -19,6 +35,14 @@ def register():
         query = mongo.db.users.find({}, {'_id': False})
         data = {'clients': list(query)}
         return jsonify(data)
+
+@app.route('/check', methods=['GET'])
+def check():
+    query = mongo.db.users.find({}, {'_id': False})
+    for client in query:
+        if not is_online(client['url']):
+            telegram_bot.notify(client['name'])
+    return jsonify({'response': 'ping clients...'})
 
 @app.route('/drop', methods=['DELETE'])
 def drop():
